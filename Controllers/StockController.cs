f36fac729f2d3901c49616575715085dd13af7cb
xavimazo <34321000+xavimazo@users.noreply.github.com>
2018-04-02 00:08:42 -0300

    Dashboard

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -6,24 +6,24 @@ using System.Web.Mvc;
 
 namespace Tupperware_e_commerce.Controllers
 {
-    public class PublicationController : Controller
+    public class StockController : Controller
     {
         // GET: Publication
         public ActionResult Create()
         {
-            return View("../Dashboard/Publication/Create");
+            return View("../Dashboard/Stock/Create");
         }
         public ActionResult Delete()
         {
-            return View("../Dashboard/Publication/Delete");
+            return View("../Dashboard/Stock/Delete");
         }
         public ActionResult Edit()
         {
-            return View("../Dashboard/Publication/Edit");
+            return View("../Dashboard/Stock/Edit");
         }
         public ActionResult Index()
         {
-            return View("../Dashboard/Publication/Index");
+            return View("../Dashboard/Stock/Index");
         }
     }
 }
